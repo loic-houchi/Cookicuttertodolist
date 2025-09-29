81d1b2c2ecdd8f5272bf76b613f503b51d387af7
loic-houchi <houchiloictchoula@gmail.com>
2025-09-29 14:26:50 +0100

    Initial commit

---
+++ gestion_taches/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from gestion_taches.users.models import User
+from gestion_taches.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
